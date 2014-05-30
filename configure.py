31c20ea989bd487ec40bee82374a91a31acf3fe9
DanBrink91 <DanBrink91@gmail.com>
2014-05-30 18:11:49 -0400

    configure.py now does multiple controllers, based on arguments given. defaults to all controllers.

--- configure.py
+++ configure.py
@@ -14,14 +14,16 @@ try:
     stick.init()
 except:
     pass
+
 #  What controller are we configuring?
-selected_controller = 1
 controllers_available = os.listdir('controllers')
 
-#  Read in controller data
-input_path = "controllers/" + controllers_available[selected_controller]
-input_text = open(input_path + "/info.json").read()
-controller = json.loads(input_text)
+if len(sys.argv) > 1:
+    controllers = sys.arg[1:]
+else:
+    controllers = controllers_available
+
+events_to_capture = [KEYUP, JOYBUTTONUP, JOYHATMOTION, JOYAXISMOTION]
 
 #  Initialize screen
 pygame.display.init()
@@ -30,13 +32,13 @@ fullscreen = False
 
 flag = 0
 if fullscreen:
-	flag = pygame.FULLSCREEN
+    flag = pygame.FULLSCREEN
 
 if (pygame.display.mode_ok((dinfo.current_w,dinfo.current_h),pygame.FULLSCREEN)):
-	windowSurface = pygame.display.set_mode((dinfo.current_w, dinfo.current_h), flag)
+    windowSurface = pygame.display.set_mode((dinfo.current_w, dinfo.current_h), flag)
 else:
-	pygame.quit()
-	sys.exit()
+    pygame.quit()
+    sys.exit()
 
 
 
@@ -44,14 +46,6 @@ else:
 
 center_x = windowSurface.get_rect().centerx
 center_y = windowSurface.get_rect().centery
-
-#  Load controller image
-controllerImage = pygame.image.load(input_path + '/'+controller['image']).convert()
-
-#  Setup where controller image will be drawn
-controllerImageRect = controllerImage.get_rect()
-controllerImageRect.centerx = center_x
-controllerImageRect.centery = center_y
 
 
 #  Setup font
@@ -80,53 +74,70 @@ def render():
     
     pygame.display.update()
 
-#  Which button are we adjusting?
-current_button = 0
-buttons_to_update = controller['controls']
-'''
-    KEYUP = scancode, key, mod
-    JOYBUTTONUP = joy, button
-    JOYHATMOTION = joy, hat, value
-'''
-events_to_capture = [KEYUP, JOYBUTTONUP, JOYHATMOTION, JOYAXISMOTION]
+for selected_controller in controllers:
+    #  Read in controller data
+    input_path = "controllers/" + selected_controller
+    try:
+        input_text = open(input_path + "/info.json").read()
+    except:
+        print "Invalid controller: %s. Skipping" % selected_controller
+        continue
+    controller = json.loads(input_text)
 
-mapping = {}
-running = True
-render()
+    #  Load controller image
+    controllerImage = pygame.image.load(input_path + '/'+controller['image']).convert()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        
-        if event.type in events_to_capture:
-            if event.type == KEYUP:
-                mapping[buttons_to_update[current_button]] = {"type":event.type, "key":event.key, "mod": event.mod}
-            
-            elif event.type == JOYBUTTONUP:
-                mapping[buttons_to_update[current_button]] = {"type":event.type, "button":event.button, "joy": event.joy}
-            
-            elif event.type == JOYHATMOTION:
-                #  Skip the event of the joystick reseting to 0, 0
-                if event.value == (0,0):
-                    continue
-                mapping[buttons_to_update[current_button]] = {"type": event.type, "value": event.value, "joy": event.joy}
-            
-            elif event.type == JOYAXISMOTION:
-                #  Skip if the press wasn't 'hard' enough
-                if event.value < 1.0 and event.value > -1.0:
-                    continue
-                mapping[buttons_to_update[current_button]] = {"type": event.type, "value": event.value, "axis": event.axis, "joy": event.joy}
-            
-            #  Advance to next button
-            current_button += 1
-            if current_button >= len(buttons_to_update):
-                current_button = 0
-                running = False
-            else:
-                render()
+    #  Setup where controller image will be drawn
+    controllerImageRect = controllerImage.get_rect()
+    controllerImageRect.centerx = center_x
+    controllerImageRect.centery = center_y
 
-#  Output our mapping
-with open(controller['name'] + ".json", "w") as output_file:
-    output_file.write(json.dumps(mapping, indent=4, separators=(',', ': ')))
+    #  Which button are we adjusting?
+    current_button = 0
+    buttons_to_update = controller['controls']
+    '''
+        KEYUP = scancode, key, mod
+        JOYBUTTONUP = joy, button
+        JOYHATMOTION = joy, hat, value
+    '''
+
+    mapping = {}
+    running = True
+    render()
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            
+            if event.type in events_to_capture:
+                if event.type == KEYUP:
+                    mapping[buttons_to_update[current_button]] = {"type":event.type, "key":event.key, "mod": event.mod}
+                
+                elif event.type == JOYBUTTONUP:
+                    mapping[buttons_to_update[current_button]] = {"type":event.type, "button":event.button, "joy": event.joy}
+                
+                elif event.type == JOYHATMOTION:
+                    #  Skip the event of the joystick reseting to 0, 0
+                    if event.value == (0,0):
+                        continue
+                    mapping[buttons_to_update[current_button]] = {"type": event.type, "value": event.value, "joy": event.joy}
+                
+                elif event.type == JOYAXISMOTION:
+                    #  Skip if the press wasn't 'hard' enough
+                    if event.value < 1.0 and event.value > -1.0:
+                        continue
+                    mapping[buttons_to_update[current_button]] = {"type": event.type, "value": event.value, "axis": event.axis, "joy": event.joy}
+                
+                #  Advance to next button
+                current_button += 1
+                if current_button >= len(buttons_to_update):
+                    current_button = 0
+                    running = False
+                else:
+                    render()
+
+    #  Output our mapping
+    with open(controller['name'] + ".json", "w") as output_file:
+        output_file.write(json.dumps(mapping, indent=4, separators=(',', ': ')))
